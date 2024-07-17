b6e1d703fcfc5a1a92a8ef45188d8a5ac2047597
aketza <aketza.eguskiza@kaytek.io>
2024-07-17 09:02:15 +0200

    weird console log overriding issue

--- server/src/electron.ts
+++ server/src/electron.ts
@@ -10,25 +10,20 @@ import PluginLoader from "./plugins/plugins";
 const { app, BrowserWindow, ipcMain } = require("electron");
 
 export class ElectronApp {
-
   private database?: Database;
   private api?: API;
 
-  public constructor() {
-
-  }
+  public constructor() {}
 
   public async start(server: boolean) {
     console.log(`api start: ${server}`);
-    
-    if(server)
-      await this.startServer();
-    
+
+    if (server) await this.startServer();
+
     await this.setEvents();
-    
   }
 
-  public async startServer(){
+  public async startServer() {
     this.title();
     await this.startDatabase();
     await this.startAPI();
@@ -36,14 +31,19 @@ export class ElectronApp {
   }
 
   public title() {
-    console.info("###############################################################################");
-    console.info("                     VALHALLA by @Akrck02 - Coffee version                     ");
-    console.info("###############################################################################");
+    console.info(
+      "###############################################################################",
+    );
+    console.info(
+      "                     VALHALLA by @Akrck02 - Coffee version                     ",
+    );
+    console.info(
+      "###############################################################################",
+    );
     console.info(" ");
   }
 
   public async startDatabase() {
-
     fs.mkdirSync(`${homedir()}/valhalla/db`, { recursive: true });
 
     this.database = new Database();
@@ -52,65 +52,57 @@ export class ElectronApp {
       console.log("Electron", "Database found");
       //fs.rmSync(databasePath);
       //console.log("Electron", "Database deleted.");
-      await this.database.createDB();    
-    }
-    else await this.database.createDB();
-
-
+      await this.database.createDB();
+    } else await this.database.createDB();
   }
 
   public startAPI() {
-
-      if(this.database){      
-        this.api = new API(this.database);
-        this.api.start();
-      } else throw new Error("[DB-API] Database not initialized, exiting...");
+    if (this.database) {
+      this.api = new API(this.database);
+      this.api.start();
+    } else throw new Error("[DB-API] Database not initialized, exiting...");
   }
 
   public startPlugins() {
+    if (!this.database)
+      throw new Error("[PLUGINS] Database not initialized, exiting...");
 
-    if(!this.database)
-      throw new Error("[PLUGINS] Database not initialized, exiting...");  
-      
     PluginLoader.load(this.database);
-
   }
 
-  public async startIpcEventListeners(){
-
-    // ipc events 
-    ipcMain.on('minimize', (event : any) => {
-      const webContents = event.sender
-      const win = BrowserWindow.fromWebContents(webContents)
-      win.minimize()
-    })
-
-    ipcMain.on('maximize', (event : any) => {
-      const webContents = event.sender
-      const win = BrowserWindow.fromWebContents(webContents)
-      win.maximize()
+  public async startIpcEventListeners() {
+    // ipc events
+    ipcMain.on("minimize", (event: any) => {
+      const webContents = event.sender;
+      const win = BrowserWindow.fromWebContents(webContents);
+      win.minimize();
     });
 
-    ipcMain.on('close', (event : any) => {
-      const webContents = event.sender
-      const win = BrowserWindow.fromWebContents(webContents)
-      win.close()
+    ipcMain.on("maximize", (event: any) => {
+      const webContents = event.sender;
+      const win = BrowserWindow.fromWebContents(webContents);
+      win.maximize();
+    });
+
+    ipcMain.on("close", (event: any) => {
+      const webContents = event.sender;
+      const win = BrowserWindow.fromWebContents(webContents);
+      win.close();
     });
   }
 
   public async loadUI() {
-
     // Create the browser window.
     const mainWindow = new BrowserWindow({
       minWidth: 1280 / 2,
       minHeight: 720 / 2,
       height: 1080,
-      width: 1920, 
+      width: 1920,
       frame: false,
       show: false,
       backgroundColor: "#fafafa",
       webPreferences: {
-        preload: path.join(__dirname, 'preload.js'),
+        preload: path.join(__dirname, "preload.js"),
         enableRemoteModule: true,
         nodeIntegration: true,
         contextIsolation: true,
@@ -118,71 +110,67 @@ export class ElectronApp {
     });
 
     mainWindow.webContents.setVisualZoomLevelLimits(1, 1);
-    mainWindow.webContents.on("new-window", function(event :Event, url: string) {
-      event.preventDefault();
-      // shell.openExternal("https://"  );
-      console.log("Electron", "Opening external link: " + url);      
-    });
+    mainWindow.webContents.on(
+      "new-window",
+      function (event: Event, url: string) {
+        event.preventDefault();
+        // shell.openExternal("https://"  );
+        console.log("Electron", "Opening external link: " + url);
+      },
+    );
 
-    // get the version parameters 
+    // get the version parameters
     const versionParameters = getVersionParameters();
     console.log("VERSION", versionParameters.VERSION);
     console.log("ENVIRONMENT", versionParameters.ENVIRONMENT);
-    
+
     let indexFile = "index.html";
     let loadingFile = "loading.html";
 
     if (versionParameters.ENVIRONMENT == ENVIRONMENT.PRODUCTION) {
       indexFile = "index-prod.html";
       loadingFile = "loading-prod.html";
-    } 
+    }
     // and load the index.html of the app.
     const url = path.join(global.root, "/web/" + indexFile);
     mainWindow.loadFile(url);
     console.log("Electron", "Opening HTML: " + url);
-    
-    mainWindow.webContents.once('dom-ready', () => {
-        setTimeout(() => {
-          mainWindow.center()
-          mainWindow.show()
-        }, 500);
-    })
+
+    mainWindow.webContents.once("dom-ready", () => {
+      setTimeout(() => {
+        mainWindow.center();
+        mainWindow.show();
+      }, 500);
+    });
   }
 
   public async setEvents() {
-
     app.whenReady().then(async () => {
-
       await this.loadUI();
-      const electronApp = this; 
+      const electronApp = this;
 
       /* Create windows */
       app.on("activate", function () {
-        if (BrowserWindow.getAllWindows().length === 0)
-          electronApp.loadUI();
+        if (BrowserWindow.getAllWindows().length === 0) electronApp.loadUI();
       });
-
-    }); 
+    });
 
     app.on("window-all-closed", function () {
-      if (process.platform !== "darwin") 
-        app.quit();
+      if (process.platform !== "darwin") app.quit();
     });
 
     await this.startIpcEventListeners();
-
   }
-
 }
 
 // Redeclaring the Nodejs global variable object
 const global = {
-  root: ''
+  root: "",
 };
 global.root = path.resolve(__dirname + "/../../");
 
 // Overriding the default console.log function
-console.log = (...msg) => {
-  console.info("[" + msg[0] + "] " + msg.slice(1));
-}
-
+// FIXME: this is breaking new instalations :(
+// console.log = (...msg) => {
+//     console.info("[" + msg[0] + "] " + msg.slice(1));
+// };
